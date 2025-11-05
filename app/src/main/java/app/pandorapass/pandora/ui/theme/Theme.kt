39346ba76c9ef864b5954577d628dfe96d3a40cb
zFlxw <hello@flxw.dev>
2025-11-05 15:21:03 +0100

    feat: add some icons and fill them when selecting the tab

--- app/src/main/java/app/pandorapass/pandora/ui/theme/Theme.kt
+++ app/src/main/java/app/pandorapass/pandora/ui/theme/Theme.kt
@@ -1,6 +1,5 @@
 package app.pandorapass.pandora.ui.theme
 
-import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
