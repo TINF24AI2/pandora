aab2427d6d1cc2eb4e77d9de5263963b5e66ba33
Juli-us-R <julirahmig@gmail.com>
2025-11-28 18:19:14 +0100

    fix: android studio adding unwanted stuff

--- app/src/main/java/app/pandorapass/pandora/ui/pages/Account.kt
+++ app/src/main/java/app/pandorapass/pandora/ui/pages/Account.kt
@@ -7,11 +7,7 @@ import androidx.compose.ui.Modifier
 import app.pandorapass.pandora.ui.viewmodels.TestVaultViewModel
 
 @Composable
-<<<<<<<< HEAD:app/src/main/java/app/pandorapass/pandora/ui/pages/Account.kt
 fun AccountPage(modifier: Modifier, viewModel: TestVaultViewModel) {
-========
-fun AccountPage(modifier: Modifier, navController: NavHostController) {
->>>>>>>> origin/feat/view-password:app/src/main/java/app/pandorapass/pandora/AccountPage.kt
 
     Text("Account", modifier = modifier)
 
