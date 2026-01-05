fc66cdea223b26334e630c3eb324372e8bf6f698
mattisvensson <matti.s.58@web.de>
2026-01-05 22:43:36 +0100

    fix: add settingsDataStore as singleton

--- app/src/main/java/app/pandorapass/pandora/PandoraApplication.kt
+++ app/src/main/java/app/pandorapass/pandora/PandoraApplication.kt
@@ -5,12 +5,17 @@ import app.pandorapass.pandora.logic.models.BiometricTokenStorage
 import app.pandorapass.pandora.logic.services.BiometricCryptoHelper
 import app.pandorapass.pandora.logic.services.CryptoService
 import app.pandorapass.pandora.logic.services.impl.CryptoServiceImpl
+import app.pandorapass.pandora.data.SettingsDataStore
 
 class PandoraApplication : Application() {
     lateinit var biometricCryptoHelper: BiometricCryptoHelper
     lateinit var biometricTokenStorage: BiometricTokenStorage
 
     lateinit var cryptoService: CryptoService
+
+    val settingsDataStore by lazy {
+        SettingsDataStore(this)
+    }
 
     override fun onCreate() {
         super.onCreate()
