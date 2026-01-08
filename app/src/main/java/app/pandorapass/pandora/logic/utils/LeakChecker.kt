fc8a9c5eb29bb7c593941f9395a7f6cff1c5141e
Juli-us-R <julirahmig@gmail.com>
2026-01-08 22:34:28 +0100

    fix: handling for when wifi isnt available

--- app/src/main/java/app/pandorapass/pandora/logic/utils/LeakChecker.kt
+++ app/src/main/java/app/pandorapass/pandora/logic/utils/LeakChecker.kt
@@ -15,26 +15,30 @@ object LeakChecker {
      * @return the amount of leaks that the password was found in.
      */
     suspend fun checkPassword(password: String): Int = withContext(Dispatchers.IO) {
-        val fullHash = sha1(password)
-        val prefix = fullHash.take(5)
-        val suffix = fullHash.substring(5).uppercase(Locale.ROOT)
+        try {
+            val fullHash = sha1(password)
+            val prefix = fullHash.take(5)
+            val suffix = fullHash.substring(5).uppercase(Locale.ROOT)
 
-        val request = Request.Builder()
-            .url("https://api.pwnedpasswords.com/range/$prefix")
-            .header("User-Agent", "Pandora-LeakChecker")
-            .build()
+            val request = Request.Builder()
+                .url("https://api.pwnedpasswords.com/range/$prefix")
+                .header("User-Agent", "Pandora-LeakChecker")
+                .build()
 
-        client.newCall(request).execute().use { response ->
-            if (!response.isSuccessful) return@withContext -1
-            val responseText = response.body?.string() ?: ""
+            client.newCall(request).execute().use { response ->
+                if (!response.isSuccessful) return@withContext -1
+                val responseText = response.body?.string() ?: ""
 
-            for (line in responseText.lines()) {
-                if (line.startsWith(suffix)) {
-                    return@withContext line.split(":")[1].trim().toInt()
+                for (line in responseText.lines()) {
+                    if (line.startsWith(suffix)) {
+                        return@withContext line.split(":")[1].trim().toInt()
+                    }
                 }
             }
+            return@withContext 0
+        } catch (e: Exception) {
+            return@withContext -1
         }
-        return@withContext 0
     }
 
     private fun sha1(input: String): String {
